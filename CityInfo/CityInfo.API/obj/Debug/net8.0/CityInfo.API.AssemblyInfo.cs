95738ae9b4805105b1c2aa5eaa7f7f922a6b052d
Joshua <gallopingbull89@gmail.com>
2024-04-26 15:28:38 -0700

    intstall serli packages for logging

--- CityInfo/CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
+++ CityInfo/CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab61cf6c15749fb490b433b43f7f764e125d6f09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54ae3d93b6e115bb151a7a40280fd4927587c6e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
