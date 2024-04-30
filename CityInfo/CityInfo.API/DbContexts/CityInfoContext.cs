ef935d2553390edf5419813a96b70b62e8cd1714
Joshua <gallopingbull89@gmail.com>
2024-04-29 20:10:57 -0700

    completed intro to entity framework
    - installed required entity packages
    - instaled sqlite to view database objects
    - created entities for cities and points of interest
    - created context for CityInfo
    - created env. variable to store connection string for dev env.

---
+++ CityInfo/CityInfo.API/DbContexts/CityInfoContext.cs
@@ -0,0 +1,86 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.DbContexts
+{
+    public class CityInfoContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; } 
+
+        public DbSet<PointOfInterest> PointsOfInterest { get; set;}
+
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<City>()
+                    .HasData(
+                new City("New York City")
+                {
+                    Id = 1,
+                    Description = "The one with the big park"
+                },
+                new City("Antwerp")
+                {
+                    Id = 2,
+                    Description = "The one with the cathedral"
+                },
+                new City("Paris")
+                {
+                    Id = 3,
+                    Description = "The one with the tower"
+                },
+                new City("Japan")
+                {
+                    Id = 4,
+                    Description = "The one with the crazys shit"
+                });
+
+            modelBuilder.Entity<PointOfInterest>()
+                    .HasData(
+                new PointOfInterest("Cental Park")
+                {
+                    Id = 1,
+                    CityId = 1,
+                    Description = "fuck"
+                },
+                new PointOfInterest("Empire State Buiilding")
+                {
+                    Id = 2,
+                    CityId = 1,
+                    Description = "my ass"
+                },
+                new PointOfInterest("Cathedrawl")
+                {
+                    Id = 3,
+                    CityId = 2,
+                    Description = "some old shit"
+                },
+                new PointOfInterest("Louve")
+                {
+                    Id = 4,
+                    CityId = 2,
+                    Description = "some french shit"
+                },
+                new PointOfInterest("Hiroshima")
+                {
+                    Id = 5,
+                    CityId = 4,
+                    Description = "boom"
+                });
+
+
+            base.OnModelCreating(modelBuilder);     
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite("connectionstring");
+        //    base.OnConfiguring(optionsBuilder); 
+        //}
+    }
+}
---
+++ CityInfo/CityInfo.API/Migrations/20240430021648_InitialDataSeedPOIFixDB.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CityInfo.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDataSeedPOIFixDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 4, "The one with the crazys shit", "Japan" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "fuck", "Cental Park" },
+                    { 2, 1, "my ass", "Empire State Buiilding" },
+                    { 3, 2, "some old shit", "Cathedrawl" },
+                    { 4, 2, "some french shit", "Louve" },
+                    { 5, 4, "boom", "Hiroshima" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
