b430ed53f4965a7ed8c04e351b61afd416d12281
Joshua <gallopingbull89@gmail.com>
2024-05-03 19:24:33 -0700

    added search functionality

--- CityInfo/CityInfo.API/Services/CityInfoRepository.cs
+++ CityInfo/CityInfo.API/Services/CityInfoRepository.cs
@@ -21,20 +21,31 @@ namespace CityInfo.API.Services
                 .ToListAsync();
         }
 
-        public async Task<IEnumerable<City>> GetCitiesAsync([FromQuery]string? name)
+        public async Task<IEnumerable<City>> GetCitiesAsync([FromQuery]string? name, [FromQuery] string? searchQuery)
         {
 
-            if(string.IsNullOrEmpty(name))
+            if(string.IsNullOrEmpty(name) && string.IsNullOrEmpty(searchQuery))
             {
                 return await GetCitiesAsync();
             }
 
-            name = name.Trim();
+            var collection = _context.Cities as IQueryable<City>;
 
-            return await _context.Cities
-                .Where(c => c.Name == name)
-                .OrderBy(c => name)
-                .ToListAsync();
+            if(!string.IsNullOrEmpty(name)) 
+            { 
+                name = name.Trim(); 
+                collection = collection.Where(c => c.Name == name);
+            }
+
+
+            if(!string.IsNullOrEmpty(searchQuery)) 
+            {
+                searchQuery = searchQuery.Trim();
+                collection = collection.Where(a => a.Name.Contains(searchQuery)
+                || (a.Description != null && a.Description.Contains(searchQuery)));
+            }
+
+            return await collection.OrderBy(c => c.Name).ToListAsync();     
         }
 
         public async Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest)
--- CityInfo/CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
+++ CityInfo/CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7404bee0ff4ac05a92c38a8f37b9295b319d5bb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fd5d09d209c4529a877da039dbfcaf8aaab3529")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
