6b342491fdd169fa892d01a82578b794b4017e53
Joshua <gallopingbull89@gmail.com>
2024-05-04 15:10:17 -0700

    added functionality to create token
    - installed jwt related packages

--- CityInfo/CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
+++ CityInfo/CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b430ed53f4965a7ed8c04e351b61afd416d12281")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+075e80cefcaa9c12af4f0d7e2556d17c0e459cb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
