cd208819e31cef10f1234bf45c0a1f2f6c4ed57d
Joshua <gallopingbull89@gmail.com>
2024-05-13 21:23:02 -0700

    adding custom response description for 200 status

--- CityInfo/CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
+++ CityInfo/CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78821858eb092c7cb0562909b8b1673acb475eaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6408437f98fc4ce88b36abe883f1f04f1d99969")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
